d3512105173478e23b4ff833f40ad9d3e9a83e47
Ida Hay Jørgensen <idahayjorgensen@idas-macbook-air.home>
2023-04-13 11:35:26 +0200

    create button component w. hover

---
+++ src/components/Button.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { defineProps, computed, ref } from "vue";
+
+const props = defineProps({
+  name: String,
+  darkImgUrl: String,
+  lightImgUrl: String,
+});
+
+const isHovering = ref(false);
+
+const imgUrl = computed(() => {
+  return isHovering.value ? props.darkImgUrl : props.lightImgUrl;
+});
+</script>
+
+<template>
+  <button
+    class="form__button flex flex__align--center"
+    @mouseover="isHovering = true"
+    @mouseleave="isHovering = false"
+  >
+    {{ props.name }}
+    <img class="button__img" :src="imgUrl" />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/main.scss";
+@import "../assets/flex.scss";
+
+.form {
+  &__button {
+    position: relative;
+    color: $contrast-light;
+    background-color: $primary-color;
+    width: 247px;
+    height: 43px;
+    font-family: "primary-font-bold";
+    border-radius: 11px;
+    border: solid 1.5px $primary-color;
+    font-size: 1.8rem;
+    padding-left: 19px;
+    margin: 0;
+    overflow: hidden;
+
+    .button__img {
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+
+    &:hover {
+      background-color: $contrast-light;
+      color: $primary-color;
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,28 @@
 <script setup>
-import Header from '/src/components/Header.vue';
-
-
+import Button from "../components/Button.vue";
+import Header from "/src/components/Header.vue";
 </script>
 
 <template>
   <div class="container">
     <Header />
     <div class="flex">
-      <aside>
-
-      </aside>
+      <aside></aside>
       <div class="content">
         <p>test</p>
+        <Button
+          name="Sanding"
+          dark-img-url="src/assets/img/sanding-dark.svg"
+          light-img-url="src/assets/img/sanding-light.svg"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-@import '../assets/main.scss';
-@import '../assets/flex.scss';
+@import "../assets/main.scss";
+@import "../assets/flex.scss";
 
 .container {
   max-width: $max-width;
@@ -33,13 +35,12 @@ aside {
   background-color: #ff1493;
 }
 
-
 .content {
   width: calc(100vw - 245px);
   height: calc(100vh - 100px);
-  background-image: url('../assets/img/white-dots.png');
+  background-image: url("../assets/img/white-dots.png");
 }
 
-
-@media only screen and (max-width: $breakpoint-sm) {}
+@media only screen and (max-width: $breakpoint-sm) {
+}
 </style>
