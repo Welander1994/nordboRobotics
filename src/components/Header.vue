738906473020c74f82256a51d0f54967bde96152
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-04-13 11:00:52 +0200

    Adding styling to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,20 +3,25 @@
 </script>
 
 <template>
-  <header class="mainheader">
+
+
+
+
+
+  <header class="mainheader flex flex__justify--between flex__align--center">
     <div class="mainheader__logo-container">
       <h1 class="mainheader__logo">
-        <img src="#" class="mainheader__img">
+        <img src="/src/assets/img/logo.jpg" class="mainheader__img">
       </h1>
 
       <div class="mainheader__lang-container">
-        <a href="#" class="mainheader__lang--selected">DK</a>
-        <a href="#" class="mainheader__lang">EN</a>
+        <a href="#" class="mainheader__lang">DK</a>
+        <a href="#" class="mainheader__lang--selected">EN</a>
       </div>
     </div>
 
     <nav class="mainheader__nav">
-      <ul>
+      <ul class="flex">
         <li><a href="#" class="mainheader__link">Solutions</a></li>
         <li><a href="#" class="mainheader__link">Partners</a></li>
         <li><a href="#" class="mainheader__link">Resources</a></li>
@@ -25,19 +30,115 @@
         <li><a href="#" class="mainheader__link--primary">Get in Touch</a></li>
       </ul>
     </nav>
+
+
+    <nav class="mainheader__mobile">
+      <ul class="flex">
+        <li><a href="#" class="mainheader__link">Solutions</a></li>
+        <li><a href="#" class="mainheader__link">Partners</a></li>
+        <li><a href="#" class="mainheader__link">Resources</a></li>
+        <li><a href="#" class="mainheader__link">About Us</a></li>
+        <li><a href="#" class="mainheader__link--secondary">Partner Portal</a></li>
+        <li><a href="#" class="mainheader__link--primary">Get in Touch</a></li>
+      </ul>
+    </nav>
+
   </header>
 </template>
 
 
 <style lang="scss" scoped>
+@import '../assets/main.scss';
+@import '../assets/flex.scss';
+
+$border-radius:7px;
+
 .mainheader {
-  height: 100px;
-  display: flex;
-  background-color: red;
+    height: 100px;
+    position: relative;
+    &__mobile {
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        transform: translateX(-100%);
 
+        &.active {
+            transform: translateX(0%);
+        }
+    }
 
-  ul {
-    display: flex;
+    &__logo-container {
+        position: relative;
+    }
+
+    &__lang-container {
+        position: absolute;
+        a {
+            position: relative;
+            &:first-of-type {
+                &:after {
+                    content: url("data:image/svg+xml,%3Csvg width='2' height='22' viewBox='0 0 2 22' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cline opacity='0.9' x1='1' y1='21.0034' x2='1' y2='0.987188' stroke='%23363973' stroke-width='0.5'/%3E%3C/svg%3E%0A");
+                    position: absolute;
+                    right: -3px;
+                }
+            }
+        }
+    }
+
+    &__lang {
+        color: $primary-color;
+        font-family: 'primary-font-medium';
+        text-decoration: none;
+        margin-right: 6px;
+        &--selected {
+            text-decoration: none;
+            color: $primary-color;
+            font-family: 'primary-font-bold';
+        }
+    }
+
+  &__nav {
+    ul {
+        list-style: none;
+        li {
+            a {
+                text-decoration: none;
+                font-family: 'secondary-font-medium';
+            }
+        }
+    }
+  }
+
+  &__link {
+    padding-right: 60px;
+    color: $primary-color;
+
+    &:last-of-type {
+        padding-right: 60px;
+    }
+   
+    &:after {
+        content: url("data:image/svg+xml,%3Csvg width='17' height='10' viewBox='0 0 17 10' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M2.57125 0.0952148L8.5 6.01105L14.4288 0.0952148L16.25 1.91646L8.5 9.66646L0.75 1.91646L2.57125 0.0952148Z' fill='%23363973'/%3E%3C/svg%3E%0A");
+        margin-left: 10px;
+    }
+
+    &--secondary { 
+        background-color: $tertiary-color;
+        padding: 12.5px 25px;
+        color: $primary-color;
+        border-radius: $border-radius;
+        margin-right: 30px;
+    }
+
+    &--primary {
+        background-color: $primary-color;
+        color: $contrast-light;
+        padding: 12.5px 25px;
+        border-radius: $border-radius;
+        
+    }
   }
 }
 </style>
