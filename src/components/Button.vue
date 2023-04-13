fa8f2d47d7dd426276be3a57263940baaa35789c
Ida Hay Jørgensen <idahayjorgensen@idas-macbook-air.home>
2023-04-13 13:45:05 +0200

    hovering effect and chaning image

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,17 +1,24 @@
 <script setup>
-import { defineProps, computed, ref } from "vue";
+import { computed, ref } from "vue";
 
 const props = defineProps({
   name: String,
-  darkImgUrl: String,
-  lightImgUrl: String,
+  imgUrl: String,
+  hoverImgUrl: String,
+  buttonSelected: Boolean,
 });
 
 const isHovering = ref(false);
+const isButtonSelected = ref(props.buttonSelected);
 
+//Changing img url via the boolean value of 'isHovering' but also seeing if the button is selected
 const imgUrl = computed(() => {
-  return isHovering.value ? props.darkImgUrl : props.lightImgUrl;
+  if (isButtonSelected.value) {
+    return props.hoverImgUrl;
+  }
+  return isHovering.value ? props.hoverImgUrl || props.imgUrl : props.imgUrl;
 });
+
 </script>
 
 <template>
@@ -19,6 +26,7 @@ const imgUrl = computed(() => {
     class="form__button flex flex__align--center"
     @mouseover="isHovering = true"
     @mouseleave="isHovering = false"
+    @click="isButtonSelected = !isButtonSelected"
   >
     {{ props.name }}
     <img class="button__img" :src="imgUrl" />
@@ -35,14 +43,15 @@ const imgUrl = computed(() => {
     color: $contrast-light;
     background-color: $primary-color;
     width: 247px;
-    height: 43px;
+    height: 53px;
     font-family: "primary-font-bold";
     border-radius: 11px;
     border: solid 1.5px $primary-color;
     font-size: 1.8rem;
     padding-left: 19px;
-    margin: 0;
+    margin: 0 0 16px 0;
     overflow: hidden;
+    box-shadow: 0px 2px 2px rgba($contrast-dark, 0.1);
 
     .button__img {
       position: absolute;
@@ -53,6 +62,47 @@ const imgUrl = computed(() => {
     &:hover {
       background-color: $contrast-light;
       color: $primary-color;
+    }
+
+    &--light{
+      background-color: $contrast-light;
+      border: solid 1.5px $contrast-light;
+      color: $primary-color;
+      box-shadow: 0px 2px 2px rgba($contrast-dark, 0.15);
+
+      &:hover {
+        background-color: $primary-color;
+        color: $contrast-light;
+      }
+    }
+
+    &--selected {
+      background-color: $contrast-light;
+      color: $primary-color;
+    }
+
+    &--rounded {
+      width: 182px;
+      height: 39px;
+      border-radius: 19.5px;
+      box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.15);
+      padding: 0;
+    }
+
+    &--rounded-light {
+      width: 182px;
+      height: 39px;
+      border-radius: 19.5px;
+      background-color: $contrast-light;
+      border: solid 1.5px $contrast-light;
+      color: $primary-color;
+      box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.15);
+      padding: 0;
+
+      &:hover {
+        background-color: $primary-color;
+        color: $contrast-light;
+      }
     }
   }
 }
