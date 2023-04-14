d6d804eb649d195e518d19ef53e6e0714ca9949b
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-04-14 09:03:15 +0200

    adding info-component + changing structure to what we planned

---
+++ src/components/Information.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+
+<template>
+        <h3 class="form__subtitle">Information</h3>
+          <div class="form__content">
+            <ul class="flex--column">
+              <li>These are the robot brands that Nordbo Robotics solutions work with.</li>
+              <li>The choice of software will depend on which brand of robot the company uses.</li>
+              <li>If the company doesn’t currently have a robot then Nordbo can help to find which 				solution will suit them best.</li>
+            </ul>
+          </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
--- src/components/form/1-type-of-robot.vue
+++ src/components/form/1-type-of-robot.vue
@@ -1,22 +1,43 @@
 <script setup>
 import Button from '../Button.vue';
+import Information from '../Information.vue';
 </script>
 
 <template>
-    <div class="form__content" id="Type-of-Robot">
-        <h1>type of robot</h1>
-        <Button class="form__button" :class="{ 'form__button--selected': isButtonSelected }" name="Sanding"
+    <section class="form__section form__section--light flex flex__gap--lg" id="Type-of-Robot">
+        <section class="form__questions flex--column">
+          <h2 class="form__title">What type of robot</h2>
+          <p class="form__text--semibold">Choose a brand</p>
+          <ul class="flex--column">
+            <Button class="form__button" :class="{ 'form__button--selected': isButtonSelected }" name="Sanding"
             hover-img-url="src/assets/img/sanding-dark.svg" img-url="src/assets/img/sanding-light.svg" />
-        <Button class="form__button--light" :class="{ 'form__button--light-selected': isButtonSelected }" name="Sanding"
-            hover-img-url="src/assets/img/sanding-light.svg" img-url="src/assets/img/sanding-dark.svg" />
-        <Button class="form__button--rounded flex__justify--center" name="Next" />
-        <Button class="form__button--rounded-light flex__justify--center" name="Next" />
+            <Button class="form__button--light" :class="{ 'form__button--light-selected': isButtonSelected }" name="Sanding"
+                hover-img-url="src/assets/img/sanding-light.svg" img-url="src/assets/img/sanding-dark.svg" />
+            <Button class="form__button--rounded flex__justify--center" name="Next" />
+            <Button class="form__button--rounded-light flex__justify--center" name="Next" />
 
-        <Button class="form__button--selected" name="selected" />
-    </div>
+            <Button class="form__button--selected" name="selected" />
+          </ul>	
+        </section>
+      
+
+        <section class="form__information flex--column">
+            <Information/>
+        </section>
+    
+
+     
+    
+    </section>
 </template>
 
 <style lang="scss" scoped>
 @import "@/assets/main.scss";
 @import "@/assets/flex.scss";
+
+.form {
+ 
+}
+
+
 </style>
