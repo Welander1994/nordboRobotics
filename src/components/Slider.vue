e6f14103971c6b73e283c99c52c666b1696af32c
Ida Hay Jørgensen <idahayjorgensen@idas-macbook-air.home>
2023-04-19 11:15:26 +0200

    change styling for the slider

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -3,10 +3,23 @@
 <template>
   <div class="slider flex flex--column flex__align--center">
     <div class="slider__range-line-container">
-      <div v-for="i in 4" :key="i" :class="'slider__range-line line-' + i"></div>
+      <div
+        v-for="i in 4"
+        :key="i"
+        :class="'slider__range-line line-' + i"
+      ></div>
     </div>
-    <input class="slider__range" type="range" min="0" max="5" value="0" steps="5" />
-    <div class="slider__description flex flex__justify--between">
+    <input
+      class="slider__range"
+      type="range"
+      min="0"
+      max="5"
+      value="0"
+      steps="5"
+    />
+    <div
+      class="slider__description flex flex__justify--between flex__align--end"
+    >
       <p>Less</p>
       <p>More</p>
     </div>
@@ -19,6 +32,7 @@
 
 .slider {
   width: 430px;
+  height: 76px;
   background-color: salmon;
   position: relative;
 
@@ -26,6 +40,9 @@
     width: 375px;
     display: flex;
     justify-content: space-evenly;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
   }
 
   &__range-line {
@@ -34,7 +51,6 @@
     background-color: $primary-color;
     z-index: 1;
     position: relative;
-    top: 0px;
     /* 
     &.line-1 {
       left: calc(100%/5);
@@ -60,18 +76,19 @@
     background-color: $primary-color;
     outline: none;
     border-radius: 15px;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
 
     &::-webkit-slider-thumb {
       -webkit-appearance: none;
       appearance: none;
-      width: 25px;
-      height: 25px;
+      width: 28px;
+      height: 28px;
       outline: none;
       background: $primary-color;
       border-radius: 50%;
       cursor: pointer;
-      display: flex;
-      justify-content: center;
     }
 
     &::-moz-range-thumb {
@@ -88,7 +105,8 @@
 
   &__description {
     width: 100%;
-    margin-top: 10px;
+    height: 66px;
+    margin-top: 20px;
     font-family: "primary-font-medium";
     font-size: 1.6rem;
     color: $primary-color;
