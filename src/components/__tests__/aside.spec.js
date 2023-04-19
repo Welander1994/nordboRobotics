c040356a6fc70d046e7d754836d92b7f0117f3ec
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2023-04-19 10:06:15 +0200

    vitest for aside-component

---
+++ src/components/__tests__/aside.spec.js
@@ -0,0 +1,29 @@
+import { describe, it, test, expect } from "vitest";
+
+import { mount } from "@vue/test-utils";
+import aside from '@/components/aside.vue'
+
+
+describe('aside', () => {
+    const wrapper = mount(aside);
+
+    test("Component exists", async () => {
+        expect(aside).toBeTruthy();
+    }),
+
+        test("Check is selected", async () => {
+            const navItems = wrapper.findAll('.formnav__link');
+            expect(navItems[0].classes()).toContain('selected');
+            expect(navItems[1].classes()).not.toContain('selected');
+            // simulate a click on the second navigation item
+            await navItems[1].trigger('click');
+            expect(navItems[0].classes()).not.toContain('selected');
+            expect(navItems[1].classes()).toContain('selected');
+        }),
+
+        test("Check only one active", async () => {
+            const navItems = wrapper.findAll('.selected');
+            expect(navItems.length).toBe(1);
+        })
+
+});
