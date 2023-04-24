18a4b906d50c63bced072537a6952dcfd136858d
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-04-24 12:50:09 +0200

    adding names

--- src/components/form/2-type-of-material.vue
+++ src/components/form/2-type-of-material.vue
@@ -1,6 +1,22 @@
 <script setup>
+import { ref, onMounted } from "vue";
 import Button from "../Button.vue";
 import Information from "../Information.vue";
+
+const steps = ref([])
+
+onMounted(() => {
+  fetch('https://nordbo-robotics-default-rtdb.europe-west1.firebasedatabase.app/steps.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // check if the data is in the expected format
+    steps.value = data
+  })
+  .catch(error => {
+    console.error(error);
+    steps.value = [{ materials: [] }]; // set a default value
+  })
+})
 
 const lists = [
   {
@@ -19,7 +35,7 @@ const lists = [
 </script>
 
 <template>
-  <section
+  <section 
     class="form__section form__section--light flex flex__gap--lg"
     id="Material"
   >
@@ -27,70 +43,13 @@ const lists = [
       <h2 class="form__title">Type of Material</h2>
       <p class="form__text--semibold form__text">Choose a brand</p>
       <ul class="flex--column">
-        <Button
-          selected-class="form__button--selected"
-          name="Sanding"
-          hover-img-url="src/assets/img/sanding-dark.svg"
-          img-url="src/assets/img/sanding-light.svg"
-        />
-        <Button
-          selected-class="form__button--light-selected"
-          class="form__button--light"
-          name="Sanding"
-          hover-img-url="src/assets/img/sanding-light.svg"
-          img-url="src/assets/img/sanding-dark.svg"
-        />
-        <Button
-          selected-class="form__button--selected"
-          name="Plastic"
-          class="form__button--icon-right-space"
-          hover-img-url="src/assets/img/plastic-dark.svg"
-          img-url="src/assets/img/plastic-light.svg"
-        />
-        <Button
-          selected-class="form__button--light-selected"
-          class="form__button--light form__button--icon-right-space"
-          name="Plastic"
-          hover-img-url="src/assets/img/plastic-light.svg"
-          img-url="src/assets/img/plastic-dark.svg"
-        />
-        <Button
-          selected-class="form__button--selected"
-          name="Other"
-          class="form__button--icon-right-space"
-          hover-img-url="src/assets/img/other-dark.svg"
-          img-url="src/assets/img/other-light.svg"
-        />
-        <Button
-          selected-class="form__button--light-selected"
-          class="form__button--light form__button--icon-right-space"
-          name="Other"
-          hover-img-url="src/assets/img/other-light.svg"
-          img-url="src/assets/img/other-dark.svg"
-        />
-        <Button
-          selected-class="form__button--selected"
-          name="50%"
-          class="form__button--icon-level"
-          hover-img-url="src/assets/img/level-dark.svg"
-          img-url="src/assets/img/level-light.svg"
-        />
-        <Button
-          selected-class="form__button--light-selected"
-          class="form__button--light form__button--icon-level"
-          name="50%"
-          hover-img-url="src/assets/img/level-light.svg"
-          img-url="src/assets/img/level-dark.svg"
-        />
-        
-        <Button
-          class="form__button--rounded flex__justify--center"
-          name="Next"
-        />
-        <Button
-          class="form__button--rounded-light flex__justify--center"
-          name="Next"
-        />
+   
+        <Button v-if="steps[1]?.materials" v-for="(materials, index) in steps[1].materials" :key="index" 
+                :selected-class="'form__button--selected'" 
+                :name="materials.name"
+                :hover-img-url="materials.imgLight" 
+                :img-url="materials.imgDark" />
+   
       </ul>
     </section>
 
