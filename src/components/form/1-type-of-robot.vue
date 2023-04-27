6dc2280c02854a185118a8bd76ac4718f9465a6b
Mads Welling <54644486+MadsWAndersen@users.noreply.github.com>
2023-04-27 08:07:41 +0200

    adding robots to buttons

--- src/components/form/1-type-of-robot.vue
+++ src/components/form/1-type-of-robot.vue
@@ -50,14 +50,14 @@ function selectButton(index) {
       <p class="form__text--semibold form__text">Choose a brand</p>
       <ul class="flex--column">
         <Button v-if="steps[0]?.robots" v-for="(robot, index) in steps[0].robots" :key="index" 
-            :name="robot"
-            :imgUrl="robot.imgUrl"
-            :hoverImgUrl="robot.hoverImgUrl"
-            :selectedClass="'form__button--selected'"
-            :index="index"
-            :isSelected="selectedIndex === index"
-            @click="selectButton(index)"
-          />
+          :name="robot"
+          :imgUrl="robot.imgUrl"
+          :hoverImgUrl="robot.hoverImgUrl"
+          :selectedClass="'form__button--selected'"
+          :index="index"
+          :isSelected="selectedIndex === index"
+          @click="selectButton(index)"
+        />
       </ul>
 
     </section>
