bbbd47cfe9c7fd6a985f58f4a2e3265b492d650c
Ida Hay Jørgensen <idahayjorgensen@idas-macbook-air.home>
2023-04-27 10:13:24 +0200

    setting up the admin panel

---
+++ src/components/Admin-navigation.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { getAuth } from "firebase/auth";
+import "@/firebase.js";
+import SignOut from "../components/Sign-out.vue";
+
+const auth = getAuth();
+</script>
+
+<template>
+  <nav class="admin-navigation flex flex__align--center flex__justify--between">
+    <h2>Welcome back {{ auth.currentUser.email }}</h2>
+    <SignOut />
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/main.scss";
+@import "@/assets/flex.scss";
+
+.admin-navigation {
+  margin: 24px;
+  font-family: "primary-font-regular";
+}
+</style>
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { reactive, onMounted } from "vue";
-import { getAuth, getIdToken  } from "firebase/auth";
+import { getAuth, getIdToken } from "firebase/auth";
 import "@/firebase.js";
+import { async } from "@firebase/util";
+import AdminNavigation from "../components/Admin-navigation.vue";
 
 const auth = getAuth();
 const products = reactive({});
+const steps = reactive({});
 const getProducts = async () => {
   try {
     const response = await fetch(window.API_URL + "/products.json", {
@@ -18,32 +21,45 @@ const getProducts = async () => {
   }
 };
 
+const getSteps = async () => {
+  try {
+    const response = await fetch(window.API_URL + "/steps.json", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+    steps.value = data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const updateProducts = async (index, value) => {
   const updateProducts = { ...products[index], name: value };
   try {
     const idToken = await getIdToken(auth.currentUser); // Get the Firebase Auth ID token for the current user
-    const response = await fetch(`${window.API_URL}/products/${index}.json?auth=${idToken}`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(updateProducts)
-    });
+    const response = await fetch(
+      `${window.API_URL}/products/${index}.json?auth=${idToken}`,
+      {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(updateProducts),
+      }
+    );
     const data = await response.json();
     console.log(data); // Log the response for debugging
-  }catch (error) {
+  } catch (error) {
     console.error(error);
     console.log(token);
   }
 };
 
-
 onMounted(() => {
   auth.onAuthStateChanged((user) => {
     if (user) {
-      // User is logged in, get the process data
       console.log(user); // log user object for debugging
       getProducts();
-    } else {
-      // User is not logged in, show error message or redirect to login page
+      getSteps();
     }
   });
 });
@@ -51,13 +67,57 @@ onMounted(() => {
 
 <template>
   <main>
-    <h1>Admin</h1>
+    <AdminNavigation />
     <div class="products">
       <li v-for="(product, index) in products.value" :key="index">
-        <input type="text" :value="product.name" @input="updateProducts(index, $event.target.value)">
+        <input
+          type="text"
+          :value="product.name"
+          @input="updateProducts(index, $event.target.value)"
+        />
+        <textarea :value="product.description[0]"></textarea>
+        <textarea :value="product.description[1]"></textarea>
+        <input
+          type="text"
+          :value="product.img"
+          @input="updateProducts(index, $event.target.value)"
+        />
+        <label>Level of automation</label>
+        <p>{{ product.levelOfAutomation }}</p>
+        <p v-for="(step, index) in steps.value[6].automation" :key="index">
+          {{ step.name }}
+        </p>
       </li>
     </div>
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/assets/main.scss";
+@import "@/assets/flex.scss";
+
+.products {
+  margin: 24px;
+  li {
+    display: flex;
+    flex-direction: column;
+    margin: 1rem;
+    border: 2px solid $primary-color;
+    border-radius: 0.5rem;
+    padding: 1rem;
+    input[type="text"],
+    textarea {
+      margin-bottom: 1rem;
+      padding: 0.5rem;
+      border: none;
+      background-color: $tertiary-color;
+      color: $primary-color;
+      border-radius: 0.5rem;
+      &:focus {
+        outline: none;
+        box-shadow: 0 0 3px $primary-color;
+      }
+    }
+  }
+}
+</style>
