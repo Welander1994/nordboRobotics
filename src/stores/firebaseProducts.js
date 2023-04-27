2a594c8e38baa3097c16751e0069b0505c38319b
Ida Hay Jørgensen <idahayjorgensen@idas-macbook-air.home>
2023-04-27 13:20:36 +0200

    setting up data from product

---
+++ src/stores/firebaseProducts.js
@@ -0,0 +1,23 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+export const useFirebaseProducts = defineStore("firebase", () => {
+
+    const productData = ref([]);
+    const productOne = ref([]);
+    const productTwo = ref([]);
+
+
+    const foo = async () => {
+        const response = await fetch('https://nordbo-robotics-default-rtdb.europe-west1.firebasedatabase.app/products.json')
+        productData.value = await response.json();
+        productOne.value = productData.value[0];
+        productTwo.value = productData.value[1];
+
+
+    }
+
+
+
+    return { productData, productOne, productTwo, foo };
+});
