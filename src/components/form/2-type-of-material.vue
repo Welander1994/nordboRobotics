3fed406632b50bb1baa79cda6a56a507bbb30ba4
Ida Hay Jørgensen <idahayjorgensen@idas-macbook-air.home>
2023-05-04 09:06:13 +0200

    fix placement for logo for better UI

--- src/components/form/2-type-of-material.vue
+++ src/components/form/2-type-of-material.vue
@@ -2,10 +2,9 @@
 import { ref, onMounted } from "vue";
 import Button from "../Button.vue";
 import Information from "../Information.vue";
-import { useFirebaseData } from '@/stores/firebaseData';
+import { useFirebaseData } from "@/stores/firebaseData";
 
 const firebaseStore = useFirebaseData();
-
 
 const selectedIndex = ref(null);
 
@@ -13,22 +12,36 @@ function selectButton(index) {
   selectedIndex.value = index;
   window.location.href = "#Type-of-process";
 }
-
 </script>
 
 <template>
-  <section class="form__section form__section--dark flex flex__gap--lg" id="Type-of-material">
+  <section
+    class="form__section form__section--dark flex flex__gap--lg"
+    id="Type-of-material"
+  >
     <div class="form__section-wrapper">
       <section class="form__questions flex--column">
         <h2 class="form__title">Which material is being used?</h2>
         <p class="form__text--semibold form__text">Choose Material</p>
         <ul class="flex--column">
-
-          <Button v-if="firebaseStore.stepsData[1]?.materials"
-            v-for="(materials, index) in firebaseStore.stepsData[1].materials" :key="index"
-            :selected-class="'form__button--selected'" :name="materials.name" :hover-img-url="materials.imgLight"
-            :img-url="materials.imgDark" :index="index" :isSelected="selectedIndex === index"
-            @mouseup="selectButton(index), firebaseStore.addToProduct('typeOfMaterial', materials.name)" />
+          <Button
+            v-if="firebaseStore.stepsData[1]?.materials"
+            v-for="(materials, index) in firebaseStore.stepsData[1].materials"
+            :key="index"
+            :selected-class="'form__button--selected'"
+            :name="materials.name"
+            :hover-img-url="materials.imgLight"
+            :img-url="materials.imgDark"
+            :index="index"
+            :isSelected="selectedIndex === index"
+            :class="{
+              'form__button--icon-right-space': [1, 3].includes(index),
+            }"
+            @mouseup="
+              selectButton(index),
+                firebaseStore.addToProduct('typeOfMaterial', materials.name)
+            "
+          />
         </ul>
       </section>
 
